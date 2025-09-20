fd174559165cd1c86b78d676536a15d8088e5355
Simby1 <kimsimby@gmail.com>
2025-09-20 18:11:48 +0100

    add multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+try:
+    number = int(input("Enter a number to see its multiplication table: "))
+
+    for i in range(1, 11):
+        product = number * i
+        print(f"{number} * {i} = {product}")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")

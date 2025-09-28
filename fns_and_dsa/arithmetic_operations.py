8089095e3370393d05c6ba47b6340203ec44c860
Simby1 <kimsimby@gmail.com>
2025-09-28 06:36:02 +0100

    refine for checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
-
+def perform_operation(num1, num2, operation):
     operation = operation.lower().strip()
 
     if operation == 'add':
